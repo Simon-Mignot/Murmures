b0fd29f5c3c60e5c1edb8fa0a7ccfbbe60e14bd4
Corentin <corentin.dumortier@epsi.fr>
2017-02-08 10:13:53 +0100

    Oublie de l'adapter

---
+++ app/src/main/java/epsi/projet/jicdsmdq/murmures/Activities/MyAdapter.java
@@ -0,0 +1,73 @@
+package epsi.projet.jicdsmdq.murmures.Activities;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import epsi.projet.jicdsmdq.murmures.Classes.Message;
+import epsi.projet.jicdsmdq.murmures.R;
+
+/**
+ * Created by C.DUMORTIER on 07/02/2017.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private Context mContext;
+    ArrayList<Message> mDataset;
+    String Pseudo;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView title;
+        public TextView pseudo;
+
+        public ViewHolder(View vue) {
+            super(vue);
+            title = (TextView) vue.findViewById(R.id.title);
+            pseudo = (TextView) vue.findViewById(R.id.pseudo);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(Context mContext, ArrayList<Message> messages, String Pseudo) {
+        this.mContext = mContext;
+        this.mDataset = messages;
+        this.Pseudo = Pseudo;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.bubblechat_right, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        return new ViewHolder(itemView);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        Message message = mDataset.get(position);
+        String pseudo = Pseudo;
+        holder.title.setText(message.getMessage());
+        holder.pseudo.setText(pseudo);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
